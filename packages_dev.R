c4bcc7bdfaee5cfbb80865c78d6b643aa5b3fc03
joelnitta <joelnitta@gmail.com>
2021-03-20 10:44:58 +0900

    Add list of packages to track for renv

---
+++ packages_dev.R
@@ -0,0 +1,10 @@
+# List packages that need to be tracked by renv
+# but don't get loaded anywhere else
+library(xaringan)
+library(xtable)
+library(sourcetools)
+library(crayon)
+library(shiny)
+library(miniUI)
+library(studioapi)
+library(showtext)
